d7fff0ce8ad51d1763664f346cb2d8bd286ad105
zeka2021 <gerasimovajeka@gmail.com>
2021-08-05 23:38:52 +0300

    add task

---
+++ src/task-2.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const users = [
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+  return new Promise((res) => {
+    const updatedUsers = allUsers.map((user) =>
+      user.name === userName ? { ...user, active: !user.active } : user
+    );
+    res(updatedUsers);
+  });
+};
+
+const logger = (updatedUsers) => console.table(updatedUsers);
+
+toggleUserState(users, "Mango").then(logger);
+toggleUserState(users, "Lux").then(logger);
